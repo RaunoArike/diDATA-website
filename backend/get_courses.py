2d7388331a0671d50a62b3839f88d46617759800
Rauno Arike <r.arike@student.tudelft.nl>
2024-05-27 22:46:36 +0200

    Fix vulnerabilities

--- backend/get_courses.py
+++ backend/get_courses.py
@@ -10,10 +10,13 @@ logger = logging.getLogger(__name__)
 def get_course_list(api_key):
     header = {"accept": "application/json", "Authorization": f"Bearer {api_key}"}
     try:
-        response = requests.get(f"https://edu.ans.app/api/v2/schools/2696/courses", headers=header)
+        response = requests.get(f"https://edu.ans.app/api/v2/schools/2696/courses", headers=header, timeout=(5, 14))
         if response.status_code == 200:
             return response.json()
         else:
             raise Exception()
-    except:
-        raise Exception()
+        
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Request failed: {e}")
+        return Response({'error': 'An error occurred while processing your request.'}, status=500)
+    
--- backend/verify.py
+++ backend/verify.py
@@ -1,10 +1,12 @@
 import requests
+from rest_framework.response import Response
 
 
 def verify_api_key(key):
     header = {"accept": "application/json", "Authorization": f"Bearer {key}"}
     try:
-        response = requests.get(f"https://edu.ans.app/api/v2/schools/2696/courses", headers=header)
+        response = requests.get(f"https://edu.ans.app/api/v2/schools/2696/courses", headers=header, timeout=(5, 14))
         return response
-    except:
-        raise Exception()
+    except requests.exceptions.RequestException as e:
+        return Response({'error': 'An error occurred while processing your request.'}, status=500)
+    
